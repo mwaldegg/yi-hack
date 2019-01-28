5d3d9236c22a76f03707ad9b34c5b3785ae2bf65
mwaldegg <m.waldegger@gmail.com>
2019-01-29 00:13:32 +0100

    Update check_motion.sh

--- sd/test/check_motion.sh
+++ sd/test/check_motion.sh
@@ -1,12 +1,52 @@
+# cat check_motion.sh
 #!/bin/sh
 # each minute, check for a new video file (which is created in case of motion detection)
 # and if found, create the appropriate file for the http server
 
+
+led() {
+    # example usage :
+    #    led -boff -yon
+    # options :
+    #    -bfast
+    #    -bon
+    #    -boff
+    #    -yfast
+    #    -yon
+    #    -yoff
+
+    # first, kill current led_ctl process
+    kill $(ps | grep led_ctl | grep -v grep | awk '{print $1}')                                                      
+    # then process                                                                                                   
+    /home/led_ctl $@ &
+                                                                                                                     
+}
+
+
+#mtime=1 #Search for Motion in last n Minutes
+
+
+echo "Starting: Motion Detection"
+led -boff -bfast
+echo "Led: Blue flashing"
+
 cd /home/hd1/record/
-while [ 1 -eq 1 ] 
+
+while [ 1 -eq 1 ]
   do
     motion_file=$(find . -type f -name "*.mp4" -mmin -1 | tail -1)
     echo "M="$motion_file
     echo $motion_file | sed "s/.\//record\//" > /home/hd1/test/http/motion
-    sleep 30
+    if [ ! -z "$motion_file" ]; then
+       echo "Motion detected wihin last minute"
+       echo "LED: blue"
+       led -boff -bon
+    else
+       echo "no Motion detected"
+       led -boff -yon
+       echo "LED: yellow"
+    fi
+
+    sleep 10
+
 done
