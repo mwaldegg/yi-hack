2461ef76f52822f4ce101787a7cdbd720fb9bad5
mwaldegg <m.waldegger@gmail.com>
2019-01-29 01:17:44 +0100

    Update check_motion.sh

--- sd/test/check_motion.sh
+++ sd/test/check_motion.sh
@@ -21,6 +21,7 @@ led() {
 
 }
 
+laststate=new
 
 #mtime=1 #Search for Motion in last n Minutes
 
@@ -35,21 +36,34 @@ cd /home/hd1/record/
 
 while [ 1 -eq 1 ]
   do
-    motion_file=$(find . -type f -name "*.mp4" -mmin -1 | tail -1)
+    motion_file=$(find . -type f -name "*.mp4" -mmin -2 | tail -1)
     echo "M="$motion_file
     echo $motion_file | sed "s/.\//record\//" > /home/hd1/test/http/motion
+    led -boff -bfast
+    echo "Led: Blue flashing"
+    sleep 1
     if [ ! -z "$motion_file" ]; then
        echo "Motion detected wihin last minute"
        echo "LED: blue"
        led -boff -bon
-       wget 'http://192.168.200.64:8080/remote?action=MONITORON'  -q > /dev/null 2&>1 #Enable MagicMirror
+       if [ $laststate == "off" ]; then
+         echo "Turning on Mirror"
+         wget 'http://192.168.200.64:8080/remote?action=MONITORON' -q & > /dev/null 2>&1 #Enable MagicMirror
+       fi
+       laststate=on
+       sleep 30
     else
        echo "no Motion detected"
        led -boff -yon
        echo "LED: yellow"
-       wget 'http://192.168.200.64:8080/remote?action=MONITOROFF'  -q > /dev/null 2&>1 #Disable MagicMirror
+       if [ $laststate == "on" ]; then
+            echo "Turning off Mirror"
+            wget 'http://192.168.200.64:8080/remote?action=MONITOROFF' -q > /dev/null 2>&1 #Disable MagicMirror
+       fi
+       laststate=off
+       sleep 10
     fi
 
-    sleep 10
+    #sleep 10
 
 done
